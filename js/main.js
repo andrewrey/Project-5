0833da7067aaaeb71c56d988d1ec52a8405b8c6b
Andrew Reynolds <andrew@nearablecorp.com>
2020-01-21 11:29:39 -0800

    javascript search completed

--- js/main.js
+++ js/main.js
@@ -7,8 +7,23 @@
   - if matched, will use show() method, if doesn't match hide() method used
   */
 
-  $(.searchInput).on("keyup", function(){
-    const $text = $(this).val();
-    console.log($text)
 
-  })
+
+$(".searchInput").on("keyup", function(){
+  const $text = $(this).val().toLowerCase();
+  console.log($text);
+
+  $(".photo-link").hide();
+
+  $(".photo-link a").each(function(index, element){
+    let $dataLink = $(this).attr('data-title').toLowerCase();
+    if ($dataLink.includes($text)){
+      $(this).parent().show();
+    }
+  });
+
+});
+
+
+    
+  
